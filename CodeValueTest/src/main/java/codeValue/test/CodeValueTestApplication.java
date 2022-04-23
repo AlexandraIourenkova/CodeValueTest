22a4a21c5f2ad52b3ecbd2225cb5831fb8b08c00
sgor9 <sgor9@LAPTOP-902O5T37.mshome.net>
2022-04-23 12:52:59 +0300

    FirstCommit

---
+++ CodeValueTest/src/main/java/codeValue/test/CodeValueTestApplication.java
@@ -0,0 +1,13 @@
+package codeValue.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeValueTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeValueTestApplication.class, args);
+	}
+
+}
---
+++ CodeValueTest/src/test/java/codeValue/test/CodeValueTestApplicationTests.java
@@ -0,0 +1,13 @@
+package codeValue.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeValueTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
